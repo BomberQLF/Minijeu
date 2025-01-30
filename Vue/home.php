677ec3f36d6dac2636075b2d61db6c36e292388f
BomberQLF <tmurphyparis@gmail.com>
2025-01-30 15:52:09 +0100

    Start Project

--- Vue/home.php
+++ Vue/home.php
@@ -1,11 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./Style/style.css">
+    <title>Battle Of Nations</title>
 </head>
+
 <body>
-    
+    <div class="home-container">
+        <div class="top">
+            <div class="upper-container">
+                <div class="title">
+                    Battle Of Nations
+                    <div class="lower-title">2 Player Game!</div>
+                </div>
+            </div>
+        </div>
+        <div class="middle">
+            <div class="middle-container">
+                <div class="player1">
+                    <div class="interaction-container">
+                        <div class="img-container">
+                            <!-- ICI APPELER L'IMG DE LA BDD - PAR DÉFAUT USA -->
+                            <img src="https://flagcdn.com/w320/us.png" alt="">
+                        </div>
+                        <div class="select-details">
+                            <form action="" method="POST">
+                                <label for="nom"></label>
+                                <select name="nom" id="select-pays1" onchange="this.form.submit()">
+                                <!-- BOUCLER ICI -->
+                                    <option value="">Change Country</option>
+                                    <option value="USA">USA</option>
+                                </select>
+                            </form>
+                            <div class="stats-container">
+                                <span>STATS</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="vs">
+                    VS
+                </div>
+                <div class="player2">
+                    <div class="interaction-container">
+                        <div class="img-container">
+                            <!-- ICI APPELER L'IMG DE LA BDD - PAR DÉFAUT USA -->
+                            <img src="https://flagcdn.com/w320/us.png" alt="">
+                        </div>
+                        <div class="select-details">
+                            <form action="" method="POST">
+                                <label for="nom"></label>
+                                <select name="nom" id="select-pays1" onchange="this.form.submit()">
+                                <!-- BOUCLER ICI -->
+                                    <option value="">Change Country</option>
+                                    <option value="USA">USA</option>
+                                </select>
+                            </form>
+                            <div class="stats-container">
+                                <span>STATS</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,62 +1,25 @@
-<!-- FAIRE UN MINIJEU SUR LE THÈME DES PAYS/NATION - CAPACITÉ MILITAIRE/NOTORIÉTÉ/PUISSANCE/DEFENSE -->
-<!-- FAIRE UN MINIJEU SUR LE THÈME DES PAYS/NATION - CAPACITÉ MILITAIRE/NOTORIÉTÉ/PUISSANCE/DEFENSE -->
-<!-- FAIRE UN MINIJEU SUR LE THÈME DES PAYS/NATION - CAPACITÉ MILITAIRE/NOTORIÉTÉ/PUISSANCE/DEFENSE -->
-<!-- FAIRE UN MINIJEU SUR LE THÈME DES PAYS/NATION - CAPACITÉ MILITAIRE/NOTORIÉTÉ/PUISSANCE/DEFENSE -->
-<!-- FAIRE UN MINIJEU SUR LE THÈME DES PAYS/NATION - CAPACITÉ MILITAIRE/NOTORIÉTÉ/PUISSANCE/DEFENSE -->
-<!-- FAIRE UN MINIJEU SUR LE THÈME DES PAYS/NATION - CAPACITÉ MILITAIRE/NOTORIÉTÉ/PUISSANCE/DEFENSE -->
-<!-- FAIRE UN MINIJEU SUR LE THÈME DES PAYS/NATION - CAPACITÉ MILITAIRE/NOTORIÉTÉ/PUISSANCE/DEFENSE -->
-<!-- FAIRE UN MINIJEU SUR LE THÈME DES PAYS/NATION - CAPACITÉ MILITAIRE/NOTORIÉTÉ/PUISSANCE/DEFENSE -->
-
-
 <?php
-$db = new PDO('mysql:host=localhost;dbname=poo_jeu', 'root', 'root');
+// Connection BDD
+$db = new PDO('mysql:host=localhost;dbname=battle_nations', 'root', 'root');
+
+// Autoloader
 function chargerClasse($classe) {
     require 'Class/'.$classe.'.php';
 }
-
 spl_autoload_register('chargerClasse');
+
+// Instancier le manager
 $monManager = new PersonnageManager($db);
-$personnages = $monManager->getAllPersonnages();
-$perso2 = $monManager->getOnePersonnageById(2);
-var_dump($perso2);
-?>
 
 
-<form action="" method="POST">
-    <label for="nom">Nom</label>
-    <input type="text" name="nom" placeholder="Nom">
-
-    <label for="atk">Dégats</label>
-    <input type="text" name="atk" placeholder="Dégats">
-
-    <label for="pv">PV</label>
-    <input type="text" name="pv" placeholder="PV">
-
-    <label for="armor">Armor</label>
-    <input type="text" name="armor">
-
-    <input type="submit">
-</form>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if (!empty($_POST["nom"]) && !empty($_POST["atk"]) && !empty($_POST["pv"]) && !empty($_POST["armor"])) {
-        $nom = htmlspecialchars($_POST["nom"]);
-        $atk = (int) $_POST["atk"];
-        $pv = (int) $_POST["pv"];
-        $armor = (int) $_POST["armor"];
-
-        $tankManager = new PersonnageManager($db);
-        $tankManager->createTank($nom, $atk, $pv, $armor);
-        
-        echo "Tank créé avec succès !";
-    } else {
-        echo "Veuillez remplir tous les champs.";
-    }
+// Controller - Action
+$action = isset($_GET['action']) ? $_GET['action'] : '';
+switch ($action) {
+    // case 'value':
+    //     # code...
+    //     break;
+    
+    default:
+        include("./Vue/home.php");
+        break;
 }
-?>
-
-<!-- FAIRE DE CE FICHIER LE CONTROLLER ET CRÉER UNE VUE QU'ON APPELLE PAR DÉFAUT -->
-<!-- FAIRE DE CE FICHIER LE CONTROLLER ET CRÉER UNE VUE QU'ON APPELLE PAR DÉFAUT -->
-<!-- FAIRE DE CE FICHIER LE CONTROLLER ET CRÉER UNE VUE QU'ON APPELLE PAR DÉFAUT -->
-<!-- FAIRE DE CE FICHIER LE CONTROLLER ET CRÉER UNE VUE QU'ON APPELLE PAR DÉFAUT -->
